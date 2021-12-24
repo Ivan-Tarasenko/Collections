6de73339a3b67f4b7b15e5bd0698466c73cc202b
Ivan-Tarasenko <80888936+Ivan-Tarasenko@users.noreply.github.com>
2021-12-24 19:24:38 +0900

    add navigationBar

--- Collections/Controllers/ArrayVC.swift
+++ Collections/Controllers/ArrayVC.swift
@@ -12,11 +12,10 @@ class ArrayVC: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemGreen
-
+        storyboard?.instantiateViewController(withIdentifier: "ArrayVC")
         // Do any additional setup after loading the view.
     }
-    
-    
+
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -24,6 +23,4 @@ class ArrayVC: UIViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
     }
-
-
 }
--- Collections/Controllers/ViewController.swift
+++ Collections/Controllers/ViewController.swift
@@ -8,18 +8,16 @@
 import UIKit
 import SnapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     private var tableView: UITableView!
-
-
-
+    private var myArray: NSArray = ["Collections", "Array", "Set", "Dictionary"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.navigationController?.navigationBar.isHidden = true
         cteateTableView()
-
     }
 
     func cteateTableView() {
@@ -27,11 +25,30 @@ class ViewController: UIViewController {
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
         tableView.dataSource = self
         tableView.delegate = self
-        self.view.addSubview(tableView)
+        view.addSubview(tableView)
 
         tableView.snp.makeConstraints { make in
-            make.top.equalToSuperview().inset(10)
+            make.top.equalToSuperview().inset(0)
             make.leading.trailing.bottom.equalToSuperview().inset(0)
         }
+    }
+// MARK: - Setting sell
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        switch indexPath.row {
+        case 0:
+            cell.textLabel?.text = "\(myArray[0])"
+            cell.textLabel?.font = UIFont.boldSystemFont(ofSize: 37)
+        case 1, 2, 3:
+            cell.textLabel?.text = "\(myArray[indexPath.row])"
+        default:
+            break
+        }
+
+        return cell
     }
 }
